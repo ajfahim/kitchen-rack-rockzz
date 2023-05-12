ef114075e5068145288d24241c527fab12d9f789
ajfahim <ajfahim52@gmail.com>
2023-05-12 16:58:30 +0600

    dont know the changes

--- server.js
+++ server.js
@@ -1,27 +1,31 @@
-const express = require('express');
+const express = require("express");
 const dotenv = require("dotenv").config();
-const colors = require('colors');
-const { errorHandler } = require('./middleware/errorMiddleware');
-const connectDB = require('./config/db');
+const cors = require("cors");
+const colors = require("colors");
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
 
-connectDB()
+connectDB();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-    res.status(200).json({ msg: "Kudos 👋 from Kitchen Rack" })
-})
+  res.status(200).json({ msg: "Kudos 👋 from Kitchen Rack" });
+});
 
-//routes 
+//routes
 app.use("/api/customers", require("./routes/customerRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
-app.use("/api/products", require("./routes/productRoutes"))
-app.use("/api/orders", require("./routes/orderRoutes"))
+app.use("/api/products", require("./routes/productRoutes"));
+app.use("/api/orders", require("./routes/orderRoutes"));
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-app.listen(port, () => console.log(`📡 Server started on port ${port}`.yellow.bold))
+app.listen(port, () =>
+  console.log(`📡 Server started on port ${port}`.yellow.bold)
+);
